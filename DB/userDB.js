baf1a6db5123d6e6cb5c8f30ef9ddcf3df5d92ec
Bhupendra <bhupendra@momentum-e.com>
2024-03-11 12:28:33 +0530

    updated vehicle data

--- DB/userDB.js
+++ DB/userDB.js
@@ -2,316 +2,7 @@ const users = [
   {
     "name": "Momentum-E",
     "email": "recruitbhupendrasinghshahi2023@gmail.com",
-    "vehicles": [
-      {
-        "VEHICLE_ID": "EV017",
-        "ODOMETER_START_READING": 10500,
-        "SOC_START": 80,
-        "SOC_END": 60,
-        "AVG_VELOCITY": 55,
-        "AC_ON_DURATION": 100,
-        "CHARGE_TYPE": "Level 1",
-        "DISTANCE": 50,
-        "AMBIENT_TEMPERATURE": 28,
-        "BATTERY_TEMPERATURE": 33,
-        "VEHICLE_NUMBER": "KA02 MN 4567",
-        "IOT_DEVICE_DETAILS": {
-          "SERIAL_NUMBER": "258369147",
-          "ID": "IOT017"
-        },
-        "LOCATION": "Bangalore, India",
-        "CHARGING_STATUS": "Not charging",
-        "Vehicle_Info": {
-          "Odometer": "28500 Km",
-          "Vehicle_Model": "Honda CB Unicorn 150",
-          "Model_Year": "2019",
-          "VIN": "L0S6JMHAMMX660426",
-          "Battery_Capacity": "50 kWh",
-          "Charging_Pattern": {
-            "Total_Energy_Consumed": "90KW",
-            "Average_SOC": "90%",
-            "Connector_Type": "Type 2",
-            "Total_Charging_Sessions": 2,
-            "Average_Charging_Rate": "Type 2"
-          },
-          "Usage": {
-            "Avg_Daily_Km_Driven": "17000 km",
-            "Temperature_High_Low": "26°C / 10°C",
-            "SOC_Range": "30% - 70%",
-            "Range_Observed_Max_Min": "85 / 85",
-            "Real_Range_Observed": "85 km",
-            "Observed_vs_EPA_WLTP_provided": "85 Km 210 Km"
-          },
-          "Battery_Health": {
-            "State_Of_Health": "97%",
-            "SoH": "97.00 %",
-            "Estimated_Degradation": "3.00 %",
-            "Battery_Chemistry": "NCA",
-            "Monthly_SOH_Data": [
-              99.62,
-              99.59,
-              99.56,
-              99.49,
-              99.46,
-              99.43,
-              99.4,
-              99.33,
-              99.31,
-              99.26,
-              99.23,
-              99.2
-            ]
-          },
-          "Connected_On": "16-02-2024",
-          "Data_Points_Collected": "0",
-          "Average_Miles_Driven": [
-            24,
-            68,
-            12,
-            79,
-            32,
-            97,
-            45,
-            59,
-            21,
-            83,
-            44,
-            73
-          ]
-        }
-      },
-      {
-        "VEHICLE_ID": "EV007",
-        "ODOMETER_START_READING": 11000,
-        "SOC_START": 90,
-        "SOC_END": 70,
-        "AVG_VELOCITY": 55,
-        "AC_ON_DURATION": 100,
-        "CHARGE_TYPE": "Level 1",
-        "DISTANCE": 55,
-        "AMBIENT_TEMPERATURE": 28,
-        "BATTERY_TEMPERATURE": 35,
-        "VEHICLE_NUMBER": "RJ14 LM 9012",
-        "IOT_DEVICE_DETAILS": {
-          "SERIAL_NUMBER": "456321987",
-          "ID": "IOT007"
-        },
-        "LOCATION": "Jaipur, India",
-        "CHARGING_STATUS": "Not charging",
-        "Vehicle_Info": {
-          "Odometer": "32000 Km",
-          "Vehicle_Model": "Hero Electric Optima",
-          "Model_Year": "2020",
-          "VIN": "B3S6JMHAMMX660426",
-          "Battery_Capacity": "50 kWh",
-          "Charging_Pattern": {
-            "Total_Energy_Consumed": "85KW",
-            "Average_SOC": "85%",
-            "Connector_Type": "CCS",
-            "Total_Charging_Sessions": 1,
-            "Average_Charging_Rate": "CCS"
-          },
-          "Usage": {
-            "Avg_Daily_Km_Driven": "14000 km",
-            "Temperature_High_Low": "26°C / 10°C",
-            "SOC_Range": "35% - 75%",
-            "Range_Observed_Max_Min": "85 / 85",
-            "Real_Range_Observed": "85 km",
-            "Observed_vs_EPA_WLTP_provided": "85 Km 210 Km"
-          },
-          "Battery_Health": {
-            "State_Of_Health": "92%",
-            "SoH": "92.00 %",
-            "Estimated_Degradation": "8.00 %",
-            "Battery_Chemistry": "NCA",
-            "Monthly_SOH_Data": [
-              99.81,
-              99.78,
-              99.75,
-              99.68,
-              99.65,
-              99.62,
-              99.59,
-              99.52,
-              99.5,
-              99.45,
-              99.42,
-              99.39
-            ]
-          },
-          "Connected_On": "01-03-2024",
-          "Data_Points_Collected": "0",
-          "Average_Miles_Driven": [
-            20,
-            73,
-            35,
-            61,
-            12,
-            81,
-            46,
-            57,
-            32,
-            94,
-            68,
-            25
-          ]
-        }
-      },
-      {
-        "VEHICLE_ID": "EV009",
-        "ODOMETER_START_READING": 10000,
-        "SOC_START": 75,
-        "SOC_END": 55,
-        "AVG_VELOCITY": 55,
-        "AC_ON_DURATION": 100,
-        "CHARGE_TYPE": "Level 1",
-        "DISTANCE": 45,
-        "AMBIENT_TEMPERATURE": 27,
-        "BATTERY_TEMPERATURE": 34,
-        "VEHICLE_NUMBER": "GJ07 XY 6789",
-        "IOT_DEVICE_DETAILS": {
-          "SERIAL_NUMBER": "852963741",
-          "ID": "IOT009"
-        },
-        "LOCATION": "Ahmedabad, India",
-        "CHARGING_STATUS": "Not charging",
-        "Vehicle_Info": {
-          "Odometer": "29000 Km",
-          "Vehicle_Model": "TVS Jupiter",
-          "Model_Year": "2020",
-          "VIN": "D5S6JMHAMMX660426",
-          "Battery_Capacity": "50 kWh",
-          "Charging_Pattern": {
-            "Total_Energy_Consumed": "85KW",
-            "Average_SOC": "85%",
-            "Connector_Type": "CCS",
-            "Total_Charging_Sessions": 1,
-            "Average_Charging_Rate": "CCS"
-          },
-          "Usage": {
-            "Avg_Daily_Km_Driven": "14000 km",
-            "Temperature_High_Low": "26°C / 10°C",
-            "SOC_Range": "30% - 70%",
-            "Range_Observed_Max_Min": "85 / 85",
-            "Real_Range_Observed": "85 km",
-            "Observed_vs_EPA_WLTP_provided": "85 Km 210 Km"
-          },
-          "Battery_Health": {
-            "State_Of_Health": "92%",
-            "SoH": "92.00 %",
-            "Estimated_Degradation": "8.00 %",
-            "Battery_Chemistry": "NCA",
-            "Monthly_SOH_Data": [
-              99.67,
-              99.64,
-              99.61,
-              99.54,
-              99.51,
-              99.48,
-              99.45,
-              99.38,
-              99.36,
-              99.31,
-              99.28,
-              99.25
-            ]
-          },
-          "Connected_On": "04-03-2024",
-          "Data_Points_Collected": "0",
-          "Average_Miles_Driven": [
-            29,
-            65,
-            41,
-            77,
-            22,
-            58,
-            12,
-            83,
-            56,
-            35,
-            98,
-            44
-          ]
-        }
-      },
-      {
-        "VEHICLE_ID": "EV001",
-        "ODOMETER_START_READING": 10000,
-        "SOC_START": 80,
-        "SOC_END": 60,
-        "AVG_VELOCITY": 60,
-        "AC_ON_DURATION": 120,
-        "CHARGE_TYPE": "Level 2",
-        "DISTANCE": 50,
-        "AMBIENT_TEMPERATURE": 25,
-        "BATTERY_TEMPERATURE": 30,
-        "VEHICLE_NUMBER": "UP80 CM 3787",
-        "IOT_DEVICE_DETAILS": {
-          "SERIAL_NUMBER": "123456789",
-          "ID": "IOT001"
-        },
-        "LOCATION": "Agra, India",
-        "CHARGING_STATUS": "Not charging",
-        "Vehicle_Info": {
-          "Odometer": "279372 Km",
-          "Vehicle_Model": "SEAT Mii",
-          "Model_Year": "2021",
-          "VIN": "J1S6JMHAMMX660426",
-          "Battery_Capacity": "75 kWh",
-          "Charging_Pattern": {
-            "Total_Energy_Consumed": "100KW",
-            "Average_SOC": "100%",
-            "Connector_Type": "ChaDeMo",
-            "Total_Charging_Sessions": 0,
-            "Average_Charging_Rate": "15 kwh"
-          },
-          "Usage": {
-            "Avg_Daily_Km_Driven": "18624.8 km",
-            "Temperature_High_Low": "12°C / 4.3°C",
-            "SOC_Range": "20% - 80%",
-            "Range_Observed_Max_Min": "120.98 / 120.98",
-            "Real_Range_Observed": "121 km",
-            "Observed_vs_EPA_WLTP_provided": "121 Km 350 Km"
-          },
-          "Battery_Health": {
-            "State_Of_Health": "100%",
-            "SoH": "100.00 %",
-            "Estimated_Degradation": "0.00 %",
-            "Battery_Chemistry": "NMC",
-            "Monthly_SOH_Data": [
-              99.88,
-              99.85,
-              99.82,
-              99.75,
-              99.72,
-              99.69,
-              99.66,
-              99.59,
-              99.57,
-              99.52,
-              99.49,
-              99.46
-            ]
-          },
-          "Connected_On": "04-03-2024",
-          "Data_Points_Collected": "0",
-          "Average_Miles_Driven": [
-            82,
-            47,
-            63,
-            12,
-            90,
-            32,
-            74,
-            18,
-            55,
-            39,
-            68,
-            25
-          ]
-        }
-      }
-    ],
+    "vehicles": [],
     "profilePictureUrl": "/uploads/image (2).png"
   },
   {
@@ -320,78 +11,305 @@ const users = [
     "vehicles": [
       {
         "VEHICLE_ID": "EV001",
-        "ODOMETER_START_READING": 10000,
-        "SOC_START": 80,
-        "SOC_END": 60,
-        "AVG_VELOCITY": 60,
-        "AC_ON_DURATION": 120,
+        "ODOMETER_START_READING": 6000,
+        "SOC_START": 85,
+        "SOC_END": 65,
+        "AVG_VELOCITY": 45,
+        "AC_ON_DURATION": 60,
         "CHARGE_TYPE": "Level 2",
-        "DISTANCE": 50,
-        "AMBIENT_TEMPERATURE": 25,
+        "DISTANCE": 35,
+        "AMBIENT_TEMPERATURE": 28,
         "BATTERY_TEMPERATURE": 30,
-        "VEHICLE_NUMBER": "UP80 CM 3787",
+        "VEHICLE_NUMBER": "KA51 AB 1234",
         "IOT_DEVICE_DETAILS": {
-          "SERIAL_NUMBER": "123456789",
+          "SERIAL_NUMBER": "789456123",
           "ID": "IOT001"
         },
-        "LOCATION": "Agra, India",
+        "LOCATION": "Bangalore, India",
         "CHARGING_STATUS": "Not charging",
         "Vehicle_Info": {
-          "Odometer": "279372 Km",
-          "Vehicle_Model": "SEAT Mii",
+          "Odometer": "16000 Km",
+          "Vehicle_Model": "Ather 450X",
           "Model_Year": "2021",
-          "VIN": "J1S6JMHAMMX660426",
-          "Battery_Capacity": "75 kWh",
+          "VIN": "ATH45X12345678901",
+          "Battery_Capacity": "3.7 kWh",
           "Charging_Pattern": {
-            "Total_Energy_Consumed": "100KW",
-            "Average_SOC": "100%",
-            "Connector_Type": "ChaDeMo",
-            "Total_Charging_Sessions": 0,
-            "Average_Charging_Rate": "15 kwh"
+            "Total_Energy_Consumed": "10 kW",
+            "Average_SOC": "82%",
+            "Connector_Type": "Type 2",
+            "Total_Charging_Sessions": 6,
+            "Average_Charging_Rate": "0.45 kW"
           },
           "Usage": {
-            "Avg_Daily_Km_Driven": "18624.8 km",
-            "Temperature_High_Low": "12°C / 4.3°C",
-            "SOC_Range": "20% - 80%",
-            "Range_Observed_Max_Min": "120.98 / 120.98",
-            "Real_Range_Observed": "121 km",
-            "Observed_vs_EPA_WLTP_provided": "121 Km 350 Km"
+            "Avg_Daily_Km_Driven": "55 km",
+            "Temperature_High_Low": "35°C / 20°C",
+            "SOC_Range": "30% - 90%",
+            "Range_Observed_Max_Min": "75 / 55",
+            "Real_Range_Observed": "65 km",
+            "Observed_vs_EPA_WLTP_provided": "65 Km 150 Km"
           },
           "Battery_Health": {
-            "State_Of_Health": "100%",
-            "SoH": "100.00 %",
-            "Estimated_Degradation": "0.00 %",
-            "Battery_Chemistry": "NMC",
+            "SoH": "98.84 %",
+            "Estimated_Degradation": "1.16 %",
+            "Battery_Chemistry": "Li-ion",
             "Monthly_SOH_Data": [
-              99.88,
-              99.85,
-              99.82,
-              99.75,
-              99.72,
-              99.69,
-              99.66,
-              99.59,
-              99.57,
-              99.52,
-              99.49,
-              99.46
+              98.88,
+              98.85,
+              98.84,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0
             ]
           },
-          "Connected_On": "06-03-2024",
-          "Data_Points_Collected": "0",
+          "Connected_On": "11-03-2024",
+          "Data_Points_Collected": "220",
           "Average_Miles_Driven": [
-            82,
-            47,
-            63,
-            12,
-            90,
-            32,
-            74,
-            18,
-            55,
-            39,
-            68,
-            25
+            50,
+            65,
+            58,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+          ]
+        }
+      },
+      {
+        "VEHICLE_ID": "EV006",
+        "ODOMETER_START_READING": 5000,
+        "SOC_START": 90,
+        "SOC_END": 70,
+        "AVG_VELOCITY": 40,
+        "AC_ON_DURATION": 45,
+        "CHARGE_TYPE": "Level 1",
+        "DISTANCE": 30,
+        "AMBIENT_TEMPERATURE": 25,
+        "BATTERY_TEMPERATURE": 28,
+        "VEHICLE_NUMBER": "KA56 LM 1234",
+        "IOT_DEVICE_DETAILS": {
+          "SERIAL_NUMBER": "123456789",
+          "ID": "IOT006"
+        },
+        "LOCATION": "Bangalore, India",
+        "CHARGING_STATUS": "Charging",
+        "Vehicle_Info": {
+          "Odometer": "10000 Km",
+          "Vehicle_Model": "TVS iCube",
+          "Model_Year": "2023",
+          "VIN": "ICUBE12345678901",
+          "Battery_Capacity": "5.1 kWh",
+          "Charging_Pattern": {
+            "Total_Energy_Consumed": "11 kW",
+            "Average_SOC": "80%",
+            "Connector_Type": "Type 1",
+            "Total_Charging_Sessions": 5,
+            "Average_Charging_Rate": "0.65 kW"
+          },
+          "Usage": {
+            "Avg_Daily_Km_Driven": "50 km",
+            "Temperature_High_Low": "30°C / 18°C",
+            "SOC_Range": "20% - 85%",
+            "Range_Observed_Max_Min": "70 / 50",
+            "Real_Range_Observed": "60 km",
+            "Observed_vs_EPA_WLTP_provided": "60 Km 140 Km"
+          },
+          "Battery_Health": {
+            "SoH": "99.00 %",
+            "Estimated_Degradation": "1.00 %",
+            "Battery_Chemistry": "Li-ion",
+            "Monthly_SOH_Data": [
+              99.15,
+              99.04,
+              99,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0
+            ]
+          },
+          "Connected_On": "11-03-2024",
+          "Data_Points_Collected": "200",
+          "Average_Miles_Driven": [
+            40,
+            25,
+            35,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+          ]
+        }
+      },
+      {
+        "VEHICLE_ID": "EV002",
+        "ODOMETER_START_READING": 5500,
+        "SOC_START": 90,
+        "SOC_END": 70,
+        "AVG_VELOCITY": 40,
+        "AC_ON_DURATION": 45,
+        "CHARGE_TYPE": "Level 1",
+        "DISTANCE": 30,
+        "AMBIENT_TEMPERATURE": 25,
+        "BATTERY_TEMPERATURE": 28,
+        "VEHICLE_NUMBER": "KA52 CD 5678",
+        "IOT_DEVICE_DETAILS": {
+          "SERIAL_NUMBER": "987654321",
+          "ID": "IOT002"
+        },
+        "LOCATION": "Mumbai, India",
+        "CHARGING_STATUS": "Charging",
+        "Vehicle_Info": {
+          "Odometer": "15500 Km",
+          "Vehicle_Model": "Ather 450X",
+          "Model_Year": "2021",
+          "VIN": "ATH45X98765432109",
+          "Battery_Capacity": "3.7 kWh",
+          "Charging_Pattern": {
+            "Total_Energy_Consumed": "12 kW",
+            "Average_SOC": "85%",
+            "Connector_Type": "Type 2",
+            "Total_Charging_Sessions": 5,
+            "Average_Charging_Rate": "0.45 kW"
+          },
+          "Usage": {
+            "Avg_Daily_Km_Driven": "50 km",
+            "Temperature_High_Low": "30°C / 18°C",
+            "SOC_Range": "25% - 80%",
+            "Range_Observed_Max_Min": "70 / 50",
+            "Real_Range_Observed": "60 km",
+            "Observed_vs_EPA_WLTP_provided": "60 Km 140 Km"
+          },
+          "Battery_Health": {
+            "SoH": "97.82 %",
+            "Estimated_Degradation": "2.18 %",
+            "Battery_Chemistry": "Li-ion",
+            "Monthly_SOH_Data": [
+              97.88,
+              97.85,
+              97.82,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0
+            ]
+          },
+          "Connected_On": "11-03-2024",
+          "Data_Points_Collected": "200",
+          "Average_Miles_Driven": [
+            40,
+            25,
+            35,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+          ]
+        }
+      },
+      {
+        "VEHICLE_ID": "EV003",
+        "ODOMETER_START_READING": 7000,
+        "SOC_START": 80,
+        "SOC_END": 60,
+        "AVG_VELOCITY": 50,
+        "AC_ON_DURATION": 55,
+        "CHARGE_TYPE": "Level 2",
+        "DISTANCE": 40,
+        "AMBIENT_TEMPERATURE": 32,
+        "BATTERY_TEMPERATURE": 35,
+        "VEHICLE_NUMBER": "KA53 EF 9012",
+        "IOT_DEVICE_DETAILS": {
+          "SERIAL_NUMBER": "456789123",
+          "ID": "IOT003"
+        },
+        "LOCATION": "Chennai, India",
+        "CHARGING_STATUS": "Not charging",
+        "Vehicle_Info": {
+          "Odometer": "17000 Km",
+          "Vehicle_Model": "Ather 450X",
+          "Model_Year": "2021",
+          "VIN": "ATH45X45678912304",
+          "Battery_Capacity": "3.7 kWh",
+          "Charging_Pattern": {
+            "Total_Energy_Consumed": "11 kW",
+            "Average_SOC": "78%",
+            "Connector_Type": "Type 2",
+            "Total_Charging_Sessions": 4,
+            "Average_Charging_Rate": "0.45 kW"
+          },
+          "Usage": {
+            "Avg_Daily_Km_Driven": "60 km",
+            "Temperature_High_Low": "38°C / 22°C",
+            "SOC_Range": "20% - 85%",
+            "Range_Observed_Max_Min": "80 / 55",
+            "Real_Range_Observed": "70 km",
+            "Observed_vs_EPA_WLTP_provided": "70 Km 160 Km"
+          },
+          "Battery_Health": {
+            "SoH": "99.85 %",
+            "Estimated_Degradation": "0.15 %",
+            "Battery_Chemistry": "Li-ion",
+            "Monthly_SOH_Data": [
+              99.88,
+              98.85,
+              99.85,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0
+            ]
+          },
+          "Connected_On": "11-03-2024",
+          "Data_Points_Collected": "250",
+          "Average_Miles_Driven": [
+            50,
+            35,
+            45,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
           ]
         }
       }
